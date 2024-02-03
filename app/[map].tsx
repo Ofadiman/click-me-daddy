400d3a9a6789de49b414fc974abc0dfb85840a25
ofadiman <ofadiman@gmail.com>
2024-02-03 21:55:56 +0100

    feature: play brug map

--- app/[map].tsx
+++ app/[map].tsx
@@ -1,23 +1,159 @@
-import { Link, Stack, useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { Pressable, View, useWindowDimensions } from "react-native";
 import { Button, Text } from "react-native-paper";
+import { Image } from "expo-image";
+import { useEffect, useRef, useState } from "react";
+import { Emote, emotes } from "@/constants/emotes";
+import { faker } from "@faker-js/faker";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export const ONE_SECOND = 1000;
+export const GAME_CONSTANTS = {
+  GAME_TIME: 3,
+};
+
+enum GameState {
+  Initial = "initial",
+  Playing = "playing",
+  Finished = "finished",
+}
+
+type GameEmote = Emote & {
+  position: {
+    x: number;
+    y: number;
+  };
+};
+
+type Game = {
+  isPlaying: boolean;
+  points: number;
+  emotes: GameEmote[];
+};
 
 export default function MapScreen() {
+  const dimensions = useWindowDimensions();
   const localSearchParams = useLocalSearchParams();
+  const insets = useSafeAreaInsets();
+  const gameRef = useRef<Game>({
+    isPlaying: true,
+    points: 0,
+    emotes: emotes[localSearchParams.map as keyof typeof emotes].map(
+      (emote) => {
+        const gamote: GameEmote = {
+          ...emote,
+          position: {
+            x: faker.number.int({
+              min: 0,
+              max: dimensions.width - emote.size.width / 2,
+            }),
+            y: faker.number.int({
+              min: 0 + insets.top,
+              max: dimensions.height - emote.size.height / 2,
+            }),
+          },
+        };
+
+        return gamote;
+      },
+    ),
+  });
+  const [currentEmote, setCurrentEmote] = useState<string>(
+    faker.helpers.arrayElement(
+      gameRef.current.emotes.map((emote) => emote.name),
+    ),
+  );
+  const [gameState, setGameState] = useState<GameState>(GameState.Initial);
+  const intervalRef = useRef<null | number>(null);
+  const [timeLeft, setTimeLeft] = useState(GAME_CONSTANTS.GAME_TIME);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      console.log(`intervalRef.current: ${intervalRef.current}`);
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+
+      setTimeLeft(GAME_CONSTANTS.GAME_TIME);
+      setGameState(GameState.Finished);
+    }
+  }, [timeLeft]);
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, []);
+
+  if (gameState === GameState.Initial) {
+    return (
+      <View>
+        <Button
+          onPress={() => {
+            setGameState(GameState.Playing);
+
+            intervalRef.current = setInterval(() => {
+              setTimeLeft((prev) => prev - 1);
+            }, ONE_SECOND);
+          }}
+        >
+          Play
+        </Button>
+      </View>
+    );
+  }
+
+  if (gameState === GameState.Finished) {
+    // TODO: Handle game finished state
+    return <Text>game ended</Text>;
+  }
 
   return (
     <>
       <Stack.Screen
         options={{ headerTitle: `Map: ${localSearchParams.map as string}` }}
       />
-      <Text>{JSON.stringify(localSearchParams)}</Text>
-      <Link
-        href={{
-          pathname: "/",
-        }}
-        asChild
-      >
-        <Button>Index Screen</Button>
-      </Link>
+      <Text>Time left: {timeLeft}</Text>
+      <Text>
+        emote: {currentEmote}, points: {gameRef.current.points}
+      </Text>
+      <View style={{ position: "relative", height: 500 }}>
+        {gameRef.current.emotes.map((emote) => {
+          return (
+            <Pressable
+              onPress={() => {
+                if (emote.name === currentEmote) {
+                  gameRef.current.points++;
+                  setCurrentEmote(
+                    faker.helpers.arrayElement(
+                      gameRef.current.emotes.map((emote) => emote.name),
+                    ),
+                  );
+                }
+              }}
+              key={emote.name}
+              style={{
+                left: emote.position.x,
+                top: emote.position.y,
+                position: "absolute",
+              }}
+            >
+              <Image
+                source={{
+                  uri: emote.uri,
+                  isAnimated: emote.isAnimated,
+                }}
+                style={{
+                  height: emote.size.height / 2,
+                  width: emote.size.width / 2,
+                }}
+              />
+            </Pressable>
+          );
+        })}
+      </View>
     </>
   );
 }
---
+++ constants/emotes.ts
@@ -0,0 +1,163 @@
+export type Emote = {
+  name: string;
+  uri: string;
+  isAnimated: boolean;
+  size: {
+    width: number;
+    height: number;
+  };
+};
+
+export const brugEmotes: Emote[] = [
+  {
+    name: "brugJam",
+    uri: "https://cdn.7tv.app/emote/6216855baff1c45709b481dc/4x.webp",
+    isAnimated: true,
+    size: {
+      width: 280,
+      height: 128,
+    },
+  },
+  {
+    name: "brugHappy",
+    uri: "https://cdn.7tv.app/emote/61fabc30b687024c3843b52d/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 188,
+      height: 128,
+    },
+  },
+  {
+    name: "brugSad",
+    uri: "https://cdn.7tv.app/emote/61fabe5c9f7bac13c42e14d0/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 188,
+      height: 128,
+    },
+  },
+  {
+    name: "brugThis",
+    uri: "https://cdn.7tv.app/emote/621683a378f6e57762f9bfe3/4x.webp",
+    isAnimated: true,
+    size: {
+      width: 280,
+      height: 128,
+    },
+  },
+  {
+    name: "brugEZ",
+    uri: "https://cdn.7tv.app/emote/61fabf88017b2a6f053af653/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 188,
+      height: 128,
+    },
+  },
+  {
+    name: "brugBox",
+    uri: "https://cdn.7tv.app/emote/61e815e841a16ffa4aa288a5/4x.webp",
+    isAnimated: true,
+    size: {
+      width: 224,
+      height: 128,
+    },
+  },
+  {
+    name: "brugChef",
+    uri: "https://cdn.7tv.app/emote/61e0a72b37dd8d85f2e0c044/4x.webp",
+    isAnimated: true,
+    size: {
+      width: 212,
+      height: 128,
+    },
+  },
+  {
+    name: "brugBedge",
+    uri: "https://cdn.7tv.app/emote/61e9379ff20dcd151f06162e/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "cmonBrug",
+    uri: "https://cdn.7tv.app/emote/615739fcb785e05aa26c071c/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "brugWokege",
+    uri: "https://cdn.7tv.app/emote/61e938ce699aab943eab978a/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "brugSalute",
+    uri: "https://cdn.7tv.app/emote/622642c5043b2a353ec880c3/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "brugDrums",
+    uri: "https://cdn.7tv.app/emote/6314c810ae7e63d8ec73f368/4x.webp",
+    isAnimated: true,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "bruggium",
+    uri: "https://cdn.7tv.app/emote/6213a36821b4d5de1447209e/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "brugStop",
+    uri: "https://cdn.7tv.app/emote/62d9bd92b33fe6c4fa9379bd/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "brugDance",
+    uri: "https://cdn.7tv.app/emote/61ea97f10aadf8d57da4ef0b/4x.webp",
+    isAnimated: true,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+  {
+    name: "brugPot",
+    uri: "https://cdn.7tv.app/emote/62bda9f168a0391cc2397c91/4x.webp",
+    isAnimated: false,
+    size: {
+      width: 128,
+      height: 128,
+    },
+  },
+];
+
+export const pepeEmotes: Emote[] = [];
+
+export const emotes = {
+  pepe: pepeEmotes,
+  brug: brugEmotes,
+};
