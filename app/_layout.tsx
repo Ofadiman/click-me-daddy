1255720cd5d018fcf4b36016e9b23734ae67cc02
ofadiman <ofadiman@gmail.com>
2024-02-03 17:13:34 +0100

    feature: install fonts and add stacked layout

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import { SplashScreen, Slot, Stack } from "expo-router";
+import { useFonts, Inter_500Medium } from "@expo-google-fonts/inter";
+import { useEffect } from "react";
+import { PaperProvider } from "react-native-paper";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function Layout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter_500Medium,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <PaperProvider>
+      <Stack />
+    </PaperProvider>
+  );
+}
